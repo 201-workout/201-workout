7a46fcf1a74d486312992e0a7ed45879d5fb4104
joshuashea <joshshea44@gmail.com>
2023-08-02 11:55:12 -0700

    refactored user data function and added user constructor

--- login.js
+++ login.js
@@ -9,42 +9,91 @@
 // });
 
 
-function saveFormToObject(formId) {
-    // Access the form using its ID
-    const form = document.getElementById(formId);
+// User constructor function
+function User(name, age, weight) {
+  this.name = name;
+  this.age = age;
+  this.weight = weight;
+}
 
-    // Create an empty object to store the form data
-    const formData = {};
-    console.log(form.elements);
-    // Iterate through all form elements
-    for (let i = 0; i < form.elements.length; i++) {
-      const element = form.elements[i];
+// Function to retrieve data from the create user form
+function getUserData() {
+  const userNameInput = document.getElementById("user-name-create");
+  const userAgeInput = document.getElementById("user-age");
+  const userWeightInput = document.getElementById("user-weight");
 
-      // Check if the element is an input or select field with a name attribute
-      if (element.tagName === 'INPUT' || element.tagName === 'SELECT') {
-        const fieldName = element.name;
-        const fieldValue = element.value;
+  const name = userNameInput.value;
+  const age = parseInt(userAgeInput.value);
+  const weight = parseInt(userWeightInput.value);
 
-        // Add the field name and value to the formData object
-        formData[fieldName] = fieldValue;
-      }
-    }
+  return new User(name, age, weight);
+}
 
-    // At this point, formData object contains all the form field values
-    // You can use it as needed, for example, you can log it to the console
-    console.log(formData);
+// Event listener for the submit button
+document.getElementById("submit-button").addEventListener("click", function (event) {
+  event.preventDefault(); // Prevent form submission
 
-    // If you want to perform any other operations with the formData object, do it here.
-  }
+  const newUser = getUserData();
+
+  // Do something with the new user data
+  console.log("New User Data:");
+  console.log(newUser);
+});
+
+
+
+
+
+
+
+
+
+// function User (name, age, weight) {
+//   self.name = name,
+//   self.age = age,
+//   self.weight = weight
+// }
+
+
+// function saveFormToObject(formId) {
+//     // Access the form using its ID
+//     const form = document.getElementById(formId);
+
+//     // Create an empty object to store the form data
+//     // const formData = {};
+//     console.log(form.elements);
+//     // Iterate through all form elements
+//     for (let i = 0; i < form.elements.length; i++) {
+//       const element = form.elements[i];
+      
+//       // Check if the element is an input or select field with a name attribute
+//       if (element.tagName === 'INPUT' || element.tagName === 'SELECT') {
+//         const fieldName = element.name;
+//         const fieldValue = element.value;
+        
+//         // Add the field name and value to the formData object
+//         // formData[fieldName] = fieldValue;
+//         let newUser[fieldName] = fieldValue;
+//       }
+//     }
+//     let newUser = User(name, age, weight);
+//     console.log(newUser);
+//     return newUser;
+
+//     // At this point, formData object contains all the form field values
+//     // You can use it as needed, for example, you can log it to the console
+
+//     // If you want to perform any other operations with the formData object, do it here.
+//   }
 
   // Example usage: Call the saveFormToObject function when the form is submitted
-  document.getElementById('login-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the form from submitting to a server
-    saveFormToObject('login-form'); // Call the function to save the form data to an object
+  // document.getElementById('login-form').addEventListener('submit', function(event) {
+  //   event.preventDefault(); // Prevent the form from submitting to a server
+  //   saveFormToObject('login-form'); // Call the function to save the form data to an object
 
-  });
-  document.getElementById('account-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the form from submitting to a server
-    saveFormToObject('account-form'); // Call the function to save the form data to an object
+  // });
+  // document.getElementById('account-form').addEventListener('submit', function(event) {
+  //   event.preventDefault(); // Prevent the form from submitting to a server
+  //   saveFormToObject('account-form'); // Call the function to save the form data to an object
 
-  });
+  // });
