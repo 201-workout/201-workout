109c9b3ab34ee1950c4544a824176d5cbc3ac045
joshuashea <joshshea44@gmail.com>
2023-08-03 09:42:02 -0700

    linked pages/changed nav layout to be the same across pages

--- page-links.js
+++ page-links.js
@@ -6,17 +6,18 @@ if (window.location.pathname === "/index.html") {
 
 }
 
-//links to exercise list page (exercise-list.html)
-document.getElementById("nav-item-list").addEventListener("click", function () {
-  window.location.href = "exercise-list.html";
-});
+// //links to exercise list page (exercise-list.html)
+// document.getElementById("nav-item-list").addEventListener("click", function () {
+//   window.location.href = "exercise-list.html";
+// });
 
-// links to user account (user-account.html)
-document.getElementById("nav-item-user").addEventListener("click", function () {
-  window.location.href = "user-account.html";
-});
+// // links to user account (user-account.html)
+// document.getElementById("nav-item-user").addEventListener("click", function () {
+//   window.location.href = "user-account.html";
+// });
 
-// links to home page (index.html)
-document.getElementById("nav-item-home").addEventListener("click", function () {
-  window.location.href = "index.html";
-});
+// // links to home page (index.html)
+// document.getElementById("nav-item-home").addEventListener("click", function () {
+//   window.location.href = "index.html";
+// });
+// console.log('DONE LOADING LINK JS');
