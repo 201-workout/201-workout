fc23c6ff3538968b0bc82e7210bf6ace3312f88d
ZIIM <johntgause12@gmail.com>
2023-08-03 19:05:54 -0700

    stuff

---
+++ user.js
@@ -0,0 +1,5 @@
+// import { User } from "./login.js";
+
+// const nameElement = document.getElementById('name');
+// const ageElement =  document.getElementById('age');
+// const weightElement = document.getElementById('weight');
