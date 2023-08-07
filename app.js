f95c9498ae14be23f4a1d1af9c5f61c31f6d5bcf
Ekaterina Khoroshilova <ek.khorosh@gmail.com>
2023-08-07 14:18:32 -0700

    combined js files

--- app.js
+++ app.js
@@ -7,12 +7,22 @@ const exerciseDetailsDiv = document.getElementById('exercise_details');
 const selectedExercisesDiv = document.getElementById('selected_exercises');
 const createWorkoutForm = document.getElementById('workoutForm');
 
+if (window.location.pathname === '/home.html') {
+  document.getElementById('create-workout').addEventListener('click', function () {
+    window.location.href = 'exercisePage.html';
+  });
+
+}
+
+
 if (createWorkoutForm) {
   createWorkoutForm.addEventListener('submit', function(e) {
     e.preventDefault();
     createWorkout();
   });
 }
+
+
 
 let selectedExercises = [];
 let dontUse = ['type', 'bodyPart'];
